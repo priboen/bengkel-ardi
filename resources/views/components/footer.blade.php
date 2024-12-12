95c4a27344961ed9d5c09f60f7044fbe6d82f464
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-12 21:50:45 +0700

    delete: move directory

--- resources/views/components/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://priboen-work.carrd.co/">Astami Tech
-            Solutions</a>
-    </div>
-    <div class="footer-right">
-        1.0.0
-    </div>
-</footer>
